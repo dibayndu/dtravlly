1cc5774baeb08e542be4038abf680c1b4570b465
Sourav Das <souravdasdip65@gmail.com>
2022-10-20 18:41:20 +0600

    modifying the mobo design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import animation  from './assets/jsons/background.json'
-import './App.css';
+import { motion } from "framer-motion";
 import Lottie from "react-lottie";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import { SocialIcon } from "react-social-icons";
+import './App.css';
+import animation from './assets/jsons/background.json';
 
 function App() {
   const defaultOptions = {
@@ -12,34 +12,53 @@ function App() {
     // here is where we will declare lottie animation
     // "animation" is what we imported before animationData: animation,
     rendererSettings: {
-       preserveAspectRatio: "xMidYMid slice",
+      preserveAspectRatio: "xMidYMid slice",
     },
- };
+  };
 
   return (
     <div className="App">
       <div className="main">
-      <div className="banner">
-        <div className="headline">
-          <h1>YOUR RIDE IS <br />
-<span>GETTING READY</span> </h1>
-        </div>
-        <div className="tagline">
-          <p>We are working hard to bring you remarkable travel experiences.</p>
-          <p>Get exclusive news and updates before the commercial launch by following our official social channels.
+        <div className="banner">
+          <div className="headline">
+            <motion.h1 initial={{
+              opacity: 0
+            }}
+              animate={{
+                opacity: 1
+              }}
+              transition={{
+                duration: 1.5,
+              }}>YOUR RIDE IS <br />
+              <span>GETTING READY</span> </motion.h1>
+          </div>
+          <motion.div initial={{
+            opacity: 0
+          }}
+            animate={{
+              opacity: 1
+            }}
+            transition={{
+              duration: 2,
+            }} className="tagline">
+            <p>We are working hard to bring you remarkable travel experiences.</p>
+            <p>Get exclusive news and updates before the commercial launch by following our official social channels.
 
-</p>
-        </div>
+            </p>
+          </motion.div>
 
-        <div className="icons">
-          <FacebookIcon />
-          <InstagramIcon />
-        </div>
+          <div className="icons">
+            {/* <FacebookIcon style={{ fontSize: '2rem' }} /> */}
+            {/* <InstagramIcon style={{ fontSize: '2rem' }} /> */}
+            <SocialIcon bgColor="rgb(58, 57, 57)" url="https://www.facebook.com/dtravelly" />
+            <SocialIcon bgColor="rgb(58, 57, 57)" url="https://www.instagram.com/dtravelly/" />
+            {/* <SocialIcon bgColor="rgb(58, 57, 57)" url="dtravelly@gmail.com" /> */}
+          </div>
 
-      </div>
-      <div className="animationbackrgound">
-        <Lottie options={defaultOptions} height={500} width={500} />
-      </div>
+        </div>
+        <div className="animationbackrgound">
+          <Lottie options={defaultOptions} />
+        </div>
       </div>
     </div>
   );
